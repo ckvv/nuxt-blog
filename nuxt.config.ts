d0b1a24849e56f9f806208ce14a355e3fe8ce25b
ckvv <k_0520@qq.com>
2024-12-17 20:02:54 +0800

    chore: 切换为lightningcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,6 +71,9 @@ export default defineNuxtConfig({
     optimizeDeps: {
       include: ['debug'],
     },
+    css: {
+      transformer: 'lightningcss',
+    },
   },
   nitro: {
     prerender: {
